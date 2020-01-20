2bcdf450f9216af14ac217a57d9fb4dcf70c7c8f
holddie <holddie@163.com>
2020-01-20 22:48:02 +0800

    feature: add dashboard/nav/table component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,30 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModel} from '@angular/forms';
+import {Routes, RouterModule} from '@angular/router';
+import {NgModule} from '@angular/core';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./my-dashboard/my-dashboard.module').then(m => m.MyDashboardModule)
+  }, {
+    path: 'table',
+    loadChildren: () => import('./my-table/my-table.module').then(m => m.MyTableModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {
+}
